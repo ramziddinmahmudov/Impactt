371b3b36a540941ef8da754ec8f9a1bf59b1c533
Ramziddin <mahmudovramziddin25@gmail.com>
2024-10-13 21:56:30 +0500

    Updateg

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
                 <p>
                     "My name is Ramziddin Mahmudov, a passionate web developer with expertise in front-end and back-end technologies like React, CSS, and Supabase. I focus on creating efficient and user-friendly digital solutions. I’m always eager to learn, collaborate, and work on challenging projects. In my free time, I enjoy exploring new tech and coding personal projects."
                 </p>
-                <a href="https://github.com/ramziddinmahmudov"><button className="btn_start">Let’s get started</button></a>
+                <a href="https://github.com/ramziddinmahmudov" target="blank"><button className="btn_start">Let’s get started</button></a>
             </section>
             <section>
                 <img src={MyImage} alt="" />
